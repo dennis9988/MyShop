30b3379c4e96c5a23d46ec02583e3237902e4499
dennis9988 <63378612+dennis9988@users.noreply.github.com>
2020-04-12 08:01:19 -0500

    added some comments

--- MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -10,29 +10,38 @@ namespace MyShop.DataAccess.InMemory
 {
     class ProductRepository
     {
+        //create a cache and call it cache
         ObjectCache cache = MemoryCache.Default;
+        //create a list of type Product, and call it products
         List<Product> products;
 
+        //constructor
         public ProductRepository()
         {
+            //if products does not exist, create it
             products = cache["products"] as List<Product>;
             if (products == null)
             {
                 products = new List<Product>();
             }
         }
+        //i think this is a method, putting products into the cache
         public void Commit()
         {
             cache["products"] = products;
 
         }
+        //add p to products, p is passed in
         public void Insert(Product p)
         {
             products.Add(p);
 
         }
+        //you can hover over shit
+        //update function, taking in product of type Product, remeber Product is a class with a bunch of shit in it
         public void Update(Product product)
         {
+            //reads like find p where p.ID equals product.Id
             Product productToUpdate = products.Find(p => p.Id == product.Id);
 
             if (productToUpdate != null)
@@ -44,6 +53,7 @@ namespace MyShop.DataAccess.InMemory
                 throw new Exception("Product not found");
             }
         }
+        //another method, this time to find shit.  
         public Product Find(string Id)
         {
             Product product = products.Find(p => p.Id == Id);
@@ -57,10 +67,12 @@ namespace MyShop.DataAccess.InMemory
                 throw new Exception("Product not found");
             }
         }
+        //i think this is an interface
         public IQueryable<Product> Collection()
         {
             return products.AsQueryable();
         }
+        //delete function
         public void Delete(string Id)
         {
             Product productToDelete = products.Find(p => p.Id == Id);
